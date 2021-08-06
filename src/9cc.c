9f0511166e43cffb121520bbcafea7a01da51fac
bybrooks <garimi0902@gmail.com>
2021-08-06 15:57:03 +0900

    implement add and sub operator

--- src/9cc.c
+++ src/9cc.c
@@ -6,11 +6,34 @@ int main(int argc, char **argv){
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
+    // ポインタpにargv[1]のアドレスを格納
+    char *p = argv[1];
 
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
+    // strtol : str -> long
+    printf("    mov rax, %ld\n", strtol(p, &p, 10));
+
+    // *p : pが文字列の最後を表す\0になったらwhile終了の意味
+    // %ld はlong型の出力フォーマット指定子
+    while (*p){
+        if (*p == '+'){
+            p++;
+            printf("    add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-'){
+            p++;
+            printf("    sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        return 1;
+    }
+
     printf("    ret\n");
     return 0;
 }
